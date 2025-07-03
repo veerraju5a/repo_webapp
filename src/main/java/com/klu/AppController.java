96d0b06e4f60dfc3752c59ad3305f31b26a24ff1
veerraju5a <68723979+veerraju5a@users.noreply.github.com>
2025-07-03 09:57:55 +0530

    Update AppController.java

--- src/main/java/com/klu/AppController.java
+++ src/main/java/com/klu/AppController.java
@@ -10,10 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin
 public class AppController {
 	
-	@GetMapping("/add/{a}/{b}")
+	@GetMapping("/sub/{a}/{b}")
 	public String add(@PathVariable("a") int a, @PathVariable("b") int b) {
 		
-		return "Addition of two numbers are:"+ (a+b);
+		return "Subtraction of two numbers are:"+ (a-b);
 	}
 
 }
